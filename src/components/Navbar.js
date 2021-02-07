7039b1a606d0cef3ebeaf26dd66234035e4fb370
Charles Stein <charles.mst@gmail.com>
2021-02-07 19:13:53 -0300

    Navbar with hackernews menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import navbar from '../styles/Navbar.module.css'
 import Link from 'next/link'
-export default function Navbar(){
+export default function Navbar() {
     return <div className={navbar.nav}>
         <div className={navbar.container}>
-            <a className={navbar.logo}>
-                <img  src={"https://zos.alipayobjects.com/rmsportal/AsASAiphPWWUJWG.png"}/>
-            </a>
-            <Link href={"/top"} ><a className={navbar.route}>Top</a></Link>
+            <Link href={"/"}>
+                <a className={navbar.logo}>
+                    <img src={"https://zos.alipayobjects.com/rmsportal/AsASAiphPWWUJWG.png"} />
+                </a>
+            </Link>
             <Link href={"/news"} ><a className={navbar.route}>News</a></Link>
+            <Link href={"/past"} ><a className={navbar.route}>Past</a></Link>
+            <Link href={"/comments"} ><a className={navbar.route}>Comments</a></Link>
+            <Link href={"/ask"} ><a className={navbar.route}>Ask</a></Link>
+            <Link href={"/jobs"} ><a className={navbar.route}>Jobs</a></Link>
         </div>
     </div>
 }
